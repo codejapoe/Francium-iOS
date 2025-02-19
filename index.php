d9c237f8c036ad154a1b29add93fe56174887119
Codejapoe <98520092+codejapoe@users.noreply.github.com>
2025-02-19 12:04:08 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,6 @@
 <?php
 $file = 'Francium.mobileconfig';
 
-// Check if the file exists
-if (!file_exists($file)) {
-    die('File not found.');
-}
-
 // Set headers to force download
 header('Content-Type: application/x-apple-aspen-config');
 header('Content-Disposition: attachment; filename="' . basename($file) . '"');
